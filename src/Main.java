e766f6706cc5ea8785b4d734c9ec3d6decc7da11
Kristian Mischke <kristianmischke@gmail.com>
2017-10-07 15:48:34 -0400

    Working example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
-public class Main{
-    public static void main(String[]args){
-        }
+import java.io.IOException;
+
+import yahoofinance.*;
+
+
+public class Main {
+    
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+		Stock stock;
+		try {
+			stock = YahooFinance.get("INTC");
+			
+			System.out.println(stock.getHistory());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//System.out.println(price);
+		
+    }
 }
