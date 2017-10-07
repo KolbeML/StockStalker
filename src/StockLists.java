895d72441febc325617c1ddc9165a5367687c5a7
lisaqvu <lisavu125@gmail.cok>
2017-10-07 19:16:16 -0400

    Read Stocks' Names

---
+++ src/StockLists.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class StockLists {
+	public static ArrayList<String> names = new ArrayList<String>();
+
+	private static String fileName = "resources/stockNames.txt";
+
+	public static void readFile() {
+		try {
+			FileReader fr = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+			String line = br.readLine();
+			while (line != null) {
+				names.add(line);
+				line = br.readLine();
+			}
+			br.close();
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
