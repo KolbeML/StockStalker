2a1101ff863804f596129f00b50fb81f31104b86
lisaqvu <lisavu125@gmail.cok>
2017-10-08 00:08:07 -0400

    Fixed the calculations

--- src/StockInfo.java
+++ src/StockInfo.java
@@ -14,112 +14,107 @@ import yahoofinance.histquotes.Interval;
 public class StockInfo {
 
 	private Calendar fromDate, toDate;
-	
+
 	Interval dateInterval;
-	
+
 	private Stock stock;
-	
+
 	private List<HistoricalQuote> history;
-	
+
 	public List<HistoricalQuote> GetHistory() {
 		return history;
 	}
-	
-	
+
 	/*
 	 * 
 	 */
 	public StockInfo(String stockName, Calendar fromDate, Calendar toDate, Interval dateInterval) {
-		
+
 		UpdateInfo(stockName, fromDate, toDate, dateInterval);
-		
+
 	}
-	
-	
+
 	public void UpdateInfo() {
-		
+
 		UpdateInfo(stock.getSymbol(), fromDate, toDate, dateInterval);
-		
+
 	}
-	
+
 	public void UpdateInfo(String stockName, Calendar fromDate, Calendar toDate, Interval dateInterval) {
-		
+
 		this.fromDate = fromDate;
 		this.toDate = toDate;
-		
+
 		this.dateInterval = dateInterval;
-		
+
 		try {
 			stock = YahooFinance.get(stockName, fromDate, toDate, dateInterval);
-			
+
 			history = stock.getHistory();
-			
-			for( Iterator<HistoricalQuote> iter = history.iterator(); iter.hasNext();){
-				
+
+			for (Iterator<HistoricalQuote> iter = history.iterator(); iter.hasNext();) {
+
 				HistoricalQuote item = iter.next();
-				
+
 				int index = history.indexOf(item);
-				if(item.getClose() == null && index == 0) {
+				if (item.getClose() == null && index == 0) {
 					// if this is the starting item, and it is null
 					// we can't use it
 					iter.remove();
-					
+
 				}
-				
+
 			}
-			
+
 			this.fromDate = history.get(0).getDate();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	
+
 	public void PrintHistory() {
-		
-		for(int i = 0; i < history.size(); i++) {
-			
+
+		for (int i = 0; i < history.size(); i++) {
+
 			Calendar date = history.get(i).getDate();
 			String sDate = date.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
 			sDate += "-" + date.get(Calendar.DATE);
 			sDate += "-" + date.get(Calendar.YEAR);
-			
+
 			BigDecimal high = history.get(i).getHigh();
 			BigDecimal low = history.get(i).getLow();
-			
+
 			System.out.println(sDate + "\t\tlow: " + low + "\t\thigh: " + high);
-			
+
 		}
-		
+
 	}
-	
+
 	/*
 	 * Get the percent difference from the last index to this index
 	 */
 	public double PercentDiff(int index) {
-		
+
 		double percent = 0.0;
-		
-		if(  index > 0 && history.get(index).getClose() != null && history.get(index - 1).getClose() != null  ) {
-			
-			double lastValue = history.get(index-1).getClose().doubleValue();
+
+		if (index > 0 && history.get(index).getClose() != null && history.get(index - 1).getClose() != null) {
+
+			double lastValue = history.get(index - 1).getClose().doubleValue();
 			double value = history.get(index).getClose().doubleValue();
-			
+
 			// calculate the percent
 			// differenceInValue divided by lastValue times
-			percent =  (value - lastValue) / lastValue;
-			
+			percent = (value - lastValue) / lastValue;
+
 		}
-		
+
 		return percent;
-		
+
 	}
-	
-	
+
 	/*
 	 * Get the profit info over time
 	 */
@@ -128,33 +123,34 @@ public class StockInfo {
 		List<Double> profits = new ArrayList<Double>();
 
 		double profit = initialInvestment;
-		for(int i = 0; i < history.size(); i++) {
+		int number = (int) (profit / history.get(0).getClose().doubleValue());
+		double leftover = profit - (double) (history.get(0).getClose().doubleValue() * number);
 
-			profit *= ( 1 + PercentDiff(i) );
-
+		for (int i = 0; i < history.size(); i++) {
+			profit = (profit - leftover) * (1 + PercentDiff(i)) + leftover;
+			if (PercentDiff(i) != 0 && leftover > history.get(i).getClose().doubleValue()) {
+				number += (int) (leftover / history.get(i).getClose().doubleValue());
+				leftover = profit - (double) number * history.get(i).getClose().doubleValue();
+			}
 			profits.add(profit);
-
 		}
 
 		return profits;
 
-
 	}
-	
-	
-	public List<Double> GetPrices() {
-		
-		List<Double> prices = new ArrayList<Double>();
-		
-		for(int i = 0; i < history.size(); i++) {
 
-			prices.add( history.get(i).getClose().doubleValue() );
+	public List<Double> GetPrices() {
+
+		List<Double> prices = new ArrayList<Double>();
+
+		for (int i = 0; i < history.size(); i++) {
+
+			prices.add(history.get(i).getClose().doubleValue());
 
 		}
 
 		return prices;
-		
+
 	}
-	
-	
+
 }
