dab549e9342192dc3d75317c18d637bb5a758df9
John Wiand <JWiand3636@gmail.com>
2017-10-07 23:28:40 -0400

    Alot of Work

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -15,8 +16,12 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
 
@@ -32,7 +37,25 @@ public class Main extends Application{
 	private ListView<String> leftStockList;
 	private ListView<String> rightStockList;
 	private LineChart<Number,Number> chart;
-
+	private ComboBox<String> startDate;
+	private ComboBox<String> endDate;
+	private ComboBox<String> interval;
+	private GridPane dataPane;
+	private TextField investInput;
+	private Text investLText;
+	private Text investRText;
+	private Text rStock;
+	private Text lStock;
+	
+	private double investment;
+	
+	private StockInfo stockLData;
+	private StockInfo stockRData;
+	
+	
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
 	@Override
 	public void start(Stage primary){
 
@@ -71,20 +94,30 @@ public class Main extends Application{
 		//Builds the Pane
 		BorderPane root = new BorderPane();
 		GridPane infoPane = new GridPane();
+		dataPane = new GridPane();
 		HBox titlePane = new HBox();
 		VBox left = new VBox();
 		VBox right = new VBox();
+		
+		investLText = new Text();
+		investRText = new Text();
+		lStock = new Text();
+		rStock = new Text();
+		
+		investInput = new TextField("0");
+		Button calculate = new Button("Calculate");
 
 		//Sets up the chart
 		final NumberAxis xAxis = new NumberAxis();
 		final NumberAxis yAxis = new NumberAxis();
-		xAxis.setLabel("Week Interval");
+		xAxis.setLabel("Time Interval");
+		xAxis.setTickMarkVisible(false);
 		yAxis.setLabel("Percentage");
 		chart = new LineChart<Number, Number>(xAxis,yAxis);
 		chart.setTitle("Stock Comparison");
 
 
-		//Test array
+
 		StockLists list = new StockLists();
 		list.readFile();
 		ArrayList<String> array = list.getStocks();
@@ -98,22 +131,25 @@ public class Main extends Application{
 		left.getChildren().add(leftStockList);
 		right.getChildren().add(rightStockList);
 
+		startDate = new ComboBox<String>();
+		endDate = new ComboBox<String>();
+		interval = new ComboBox<String>();
 
 		leftStockList.getSelectionModel().selectedItemProperty().addListener(e->{
 			for(Integer i: leftStockList.getSelectionModel().getSelectedIndices()) {
 				if(array.get(i).equals(leftStockList.getSelectionModel().getSelectedItem())) {
-					ArrayList<Integer> list1 = new ArrayList<Integer>();
-					list1.add(1);
-					list1.add(2);
-					list1.add(3);
-					list1.add(4);
-					ArrayList<Double> list2 = new ArrayList<Double>();
-					list2.add(10.2);
-					list2.add(5.6);
-					list2.add(3.9);
-					list2.add(4.8);
 
-					setLeftData(array.get(i),list1,list2);
+					Calendar start = Calendar.getInstance();
+					start.add(Calendar.YEAR, -3);
+					Calendar end = Calendar.getInstance();
+					stockLData = new StockInfo(array.get(i),start,end,Interval.MONTHLY);
+					ArrayList<Double> list2 = new ArrayList<Double>(stockLData.GetPrices());
+					
+					lStock.setText(array.get(i));
+					ArrayList<Double> profits = new ArrayList<Double>(stockLData.GetProfitInfo(investment));
+					investLText.setText(profits.get(profits.size()-1)+"");
+					
+					leftStock = setData(array.get(i),list2,leftStock);
 				}
 			}
 		});
@@ -121,39 +157,49 @@ public class Main extends Application{
 		rightStockList.getSelectionModel().selectedItemProperty().addListener(e->{
 			for(Integer i: rightStockList.getSelectionModel().getSelectedIndices()) {
 				if(array.get(i).equals(rightStockList.getSelectionModel().getSelectedItem())) {
-					ArrayList<Integer> list1 = new ArrayList<Integer>();
-					list1.add(1);
-					list1.add(2);
-					list1.add(3);
-					list1.add(4);
-					ArrayList<Double> list2 = new ArrayList<Double>();
-					list2.add(8.2);
-					list2.add(13.6);
-					list2.add(6.9);
-					list2.add(9.8);
+					Calendar start = Calendar.getInstance();
+					start.add(Calendar.YEAR, -3);
+					Calendar end = Calendar.getInstance();
+					stockRData = new StockInfo(array.get(i),start,end,Interval.MONTHLY);
+					ArrayList<Double> list2 = new ArrayList<Double>(stockRData.GetPrices());
+					
 
-					setRightData(array.get(i),list1,list2);
+					rStock.setText(array.get(i));
+					ArrayList<Double> profits = new ArrayList<Double>(stockRData.GetProfitInfo(investment));
+					investRText.setText(profits.get(profits.size()-1)+"");
+					
+					rightStock = setData(array.get(i),list2,rightStock);
+
 				}
 			}
 		});
 
-
-		//Setting Data values into the Chart
-		leftStock.setName("Stuff");
-		leftStock.getData().add(new XYChart.Data(1,2));
-		leftStock.getData().add(new XYChart.Data(2,3));
-		leftStock.getData().add(new XYChart.Data(3,6));
-		leftStock.getData().add(new XYChart.Data(4,3));
-		rightStock.setName("More Stuff");
-		rightStock.getData().add(new XYChart.Data(1,6));
-		rightStock.getData().add(new XYChart.Data(2,5));
-		rightStock.getData().add(new XYChart.Data(3,3));
-		rightStock.getData().add(new XYChart.Data(4,1));
-		chart.getData().add(leftStock);
-		chart.getData().add(rightStock);
-
+		
+		dataPane.add(new Text("Invest Amount"), 0, 0);
+		dataPane.add(investInput, 0, 1);
+		dataPane.add(calculate, 0, 2);
+		dataPane.add(lStock, 0, 3);
+		dataPane.add(rStock, 0, 4);
+		dataPane.add(investLText, 1, 3);
+		dataPane.add(investRText, 1, 4);
+		
+		calculate.setOnAction(e->{
+			if(Main.isNumber(investInput.getText())) {
+				investment = Double.parseDouble(investInput.getText());
+				
+				ArrayList<Double> profitR = new ArrayList<Double>(stockRData.GetProfitInfo(investment));
+				ArrayList<Double> profitL = new ArrayList<Double>(stockLData.GetProfitInfo(investment));
+				investRText.setText(profitR.get(profitR.size()-1)+"");
+				investLText.setText(profitL.get(profitL.size()-1)+"");				
+			}
+		});
+		
 		//infoPane.add(new Text("test"), x, y);
-		infoPane.add(chart, 1, 0);
+		infoPane.add(chart, 1, 1);
+		infoPane.add(startDate, 0, 0);
+		infoPane.add(endDate, 2, 0);
+		infoPane.add(interval, 1, 0);
+		infoPane.setHalignment(interval,HPos.CENTER);
 
 		titlePane.getChildren().add(new Text("Stock Stocker"));
 		titlePane.setAlignment(Pos.CENTER);
@@ -162,34 +208,46 @@ public class Main extends Application{
 		root.setCenter(infoPane);
 		root.setRight(right);
 		root.setLeft(left);
+		root.setBottom(dataPane);
 
 
-		primary.setScene(new Scene(root,800,250));
+		primary.setScene(new Scene(root,1000,600));
 		primary.show();
 	}
 
-	protected void setLeftData(String name, ArrayList<Integer> time, ArrayList<Double> data) {
-		chart.getData().remove(leftStock);
-		leftStock = new XYChart.Series();
-		leftStock.setName(name);
-		for(int i = 0; i < time.size(); i++) {
-			leftStock.getData().add(new XYChart.Data(time.get(i),data.get(i)));
+	protected XYChart.Series setData(String name, ArrayList<Double> data, XYChart.Series series) {
+		chart.getData().remove(series);
+		series = new XYChart.Series();
+		series.setName(name);
+		for(int i = 0; i < data.size(); i++) {
+			series.getData().add(new XYChart.Data(i,data.get(i)));
 		}
 
-		chart.getData().add(leftStock);
-
+		chart.getData().add(series);
+		
+		return series;
 	}
+	
+    static boolean isNumber(String s){
 
-	protected void setRightData(String name, ArrayList<Integer> time, ArrayList<Double> data) {
-		chart.getData().remove(rightStock);
-		rightStock = new XYChart.Series();
-		rightStock.setName(name);
-		for(int i = 0; i < time.size(); i++) {
-			rightStock.getData().add(new XYChart.Data(time.get(i),data.get(i)));
-		}
+        char a;
+        int c = 0;
 
-		chart.getData().add(rightStock);
-	}
+        for(int i = 0; i < s.length(); i++){
+
+            a = s.charAt(i);
+
+            if(a == '.') {
+                continue;
+            }else if(!Character.isDigit(a))
+                c++;
+        }
+
+        if(s.length() == 0)
+            c++;
+
+        return c == 0;
+    }
 
 	public static void main(String[]args){launch(args);}
 
