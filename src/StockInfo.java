38c1e04ea7e37ea7b2d23eaa0244a4bb45d80a4e
Lisa Vu <lisavu125@gmail.com>
2017-10-08 11:24:54 -0400

    Cleaning things up

--- src/StockInfo.java
+++ src/StockInfo.java
@@ -14,40 +14,27 @@ import yahoofinance.histquotes.Interval;
 public class StockInfo {
 
 	private Calendar fromDate, toDate;
-
 	Interval dateInterval;
-
 	private Stock stock;
-
 	private List<HistoricalQuote> history;
 
 	public List<HistoricalQuote> getHistory() {
 		return history;
 	}
-	
+
 	public String getName() {
-		
 		return stock.getName();
-		
 	}
 
-	/*
-	 * 
-	 */
 	public StockInfo(String stockName, Calendar fromDate, Calendar toDate, Interval dateInterval) {
-
 		updateInfo(stockName, fromDate, toDate, dateInterval);
-
 	}
 
 	public void updateInfo() {
-
 		updateInfo(stock.getSymbol(), fromDate, toDate, dateInterval);
-
 	}
 
 	public void updateInfo(String stockName, Calendar fromDate, Calendar toDate, Interval dateInterval) {
-
 		this.fromDate = fromDate;
 		this.toDate = toDate;
 
@@ -67,7 +54,6 @@ public class StockInfo {
 					// if this is the starting item, and it is null
 					// we can't use it
 					iter.remove();
-
 				}
 
 			}
@@ -99,9 +85,7 @@ public class StockInfo {
 
 	}
 
-	/*
-	 * Get the percent difference from the last index to this index
-	 */
+	// Get the percent difference from the last index to this index
 	public double percentDiff(int index) {
 
 		double percent = 0.0;
@@ -111,8 +95,7 @@ public class StockInfo {
 			double lastValue = history.get(index - 1).getClose().doubleValue();
 			double value = history.get(index).getClose().doubleValue();
 
-			// calculate the percent
-			// differenceInValue divided by lastValue times
+			// Calculate the percent change
 			percent = getPercentChange(lastValue, value);
 
 		}
@@ -121,19 +104,19 @@ public class StockInfo {
 
 	}
 
-	/*
-	 * Get the profit info over time
-	 */
+	// Get profit over the time period
 	public List<Double> getProfitInfo(double initialInvestment) {
 
 		List<Double> profits = new ArrayList<Double>();
 
+		// Finding the number of stocks the investment can get
 		double profit = initialInvestment;
 		int number = (int) (profit / history.get(0).getClose().doubleValue());
 		double leftover = getValueChange((double) (history.get(0).getClose().doubleValue() * number), profit);
 
 		for (int i = 0; i < history.size(); i++) {
 			profit = (profit - leftover) * (1 + percentDiff(i)) + leftover;
+			// If the leftover amount is enough to buy more stocks, execute the command
 			if (percentDiff(i) != 0 && leftover > history.get(i).getClose().doubleValue()) {
 				number += (int) (leftover / history.get(i).getClose().doubleValue());
 				leftover = getValueChange((double) (history.get(i).getClose().doubleValue() * number), profit);
@@ -142,40 +125,8 @@ public class StockInfo {
 		}
 
 		return profits;
-		
-		//User is able to add the percentage rise/drop in order to sell/buy stocks.
-		//When the price hits too low (max percentage dropped), sell all.
-		
-		/*List<Double> profits = new ArrayList<Double>();
-
-		double setDrop = 0.02;
-		double setRise = 0.02;
-		double dropMax = 10.0;
-		
-		double profit = initialInvestment;
-		int number = 0;
-		double leftover =  getValueChange((double) (history.get(0).getClose().doubleValue() * number), profit);
-
-		for (int i = 0; i < history.size(); i++) {
-			profit = (profit - leftover) * (1 + percentDiff(i)) + leftover;
-			if (percentDiff(i) >= setRise || percentDiff(i) <= dropMax) {
-				number = 0;
-				if (percentDiff(i) != 0)
-					leftover = getValueChange((double) (history.get(i).getClose().doubleValue() * number), profit);
-			}
-			if (percentDiff(i) <= -setDrop) {
-				number += (int) (leftover / history.get(i).getClose().doubleValue());
-				if (percentDiff(i) != 0)
-					leftover = getValueChange((double) (history.get(i).getClose().doubleValue() * number), profit);
-			}
-			profits.add(profit);
-		}
-
-		return profits;*/
-
 
 	}
-
 
 	public List<Double> getPercentChanges() {
 
@@ -183,32 +134,25 @@ public class StockInfo {
 
 		double percent = percentDiff(0);
 		for (int i = 0; i < history.size(); i++) {
-
 			percent = percentDiff(i);
-
 			percents.add(percent);
-
 		}
 
 		return percents;
 
 	}
-	
+
 	public List<Double> getPrices() {
 
 		List<Double> prices = new ArrayList<Double>();
 
-		
-		double lastPrice = history.get(0).getClose().doubleValue();;
+		double lastPrice = history.get(0).getClose().doubleValue();
 		for (int i = 0; i < history.size(); i++) {
-
-			if( history.get(i).getClose() != null ) {
+			if (history.get(i).getClose() != null)
 				lastPrice = history.get(i).getClose().doubleValue();
-			}
-			
 			prices.add(lastPrice);
-
 		}
+
 		return prices;
 	}
 
@@ -217,7 +161,7 @@ public class StockInfo {
 	}
 
 	public static double getPercentChange(double initial, double fin) {
-		return getValueChange(initial, fin)/initial;
+		return getValueChange(initial, fin) / initial;
 	}
 
 }
